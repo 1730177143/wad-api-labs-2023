6d14e7c87039df5a3c1a8739aa9c06e04429a109
Long Liu <1730177143@qq.com>
2023-12-05 13:51:30 +0000

    Initialise Dev Env - Data

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -9,7 +9,7 @@ const UserSchema = new Schema({
 
 const passwordValidator = (password) => {
     const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-    return regex.test(password);    
+    return regex.test(password);
 }
 UserSchema.path("password").validate(passwordValidator);
 export default mongoose.model('User', UserSchema);
